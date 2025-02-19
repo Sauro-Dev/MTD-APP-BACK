c1542d6d6e55ef0b59e0e98574a243d41572487d
Bryan Marin Yupanqui <Bryan>
2025-02-19 15:37:44 -0500

    feat: Validadores mejorados y mejor uso de Lombok

--- src/main/java/com/makethediference/mtdapi/service/UserFactory.java
+++ src/main/java/com/makethediference/mtdapi/service/UserFactory.java
@@ -4,15 +4,11 @@ import com.makethediference.mtdapi.domain.entity.*;
 
 public class UserFactory {
     public static User createUser(Role role) {
-        User user = switch (role) {
+        return switch (role) {
             case ADMIN -> new Admin();
             case MAKER -> new Maker();
             case COUNCIL -> new Council();
             case COORDINATOR -> new Coordinator();
         };
-
-        user.setRole(role);
-        return user;
     }
 }
-
