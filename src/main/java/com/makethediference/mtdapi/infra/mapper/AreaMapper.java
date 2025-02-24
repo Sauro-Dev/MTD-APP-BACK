675d05ab7966cd68f4691477d2861a2a896de1ce
Bryan Marin Yupanqui <Bryan>
2025-02-24 13:51:49 -0500

    feat: Registro de Areas completado

--- src/main/java/com/makethediference/mtdapi/infra/mapper/AreaMapper.java
+++ src/main/java/com/makethediference/mtdapi/infra/mapper/AreaMapper.java
@@ -1,4 +1,13 @@
 package com.makethediference.mtdapi.infra.mapper;
 
+import com.makethediference.mtdapi.domain.dto.area.RegisterArea;
+import com.makethediference.mtdapi.domain.entity.Area;
+import org.springframework.stereotype.Component;
+
+@Component
 public class AreaMapper {
+
+    public Area toEntity(RegisterArea dto) {
+        return new Area(null, dto.name(), dto.color(), null, null);
+    }
 }
