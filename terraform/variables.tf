d93565d88b41c7fc84eb2f3661aba3436cc097de
bryan <bryanmarin2003@hotmail.com>
2025-06-19 05:12:06 -0500

    fix: Unificación de archivos terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,21 @@
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_key" {
+  description = "AWS Secret Key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+  default     = "us-east-2"
+}
+
 variable "cloudflare_api_token" {
   description = "The API token for Cloudflare"
   type        = string
@@ -11,9 +29,11 @@ variable "cloudflare_account_id" {
 variable "cloudflare_r2_bucket_name" {
   description = "The name of the R2 bucket"
   type        = string
+  default     = "mtd-files"
 }
 
 variable "cloudflare_d1_database_name" {
   description = "The name of the D1 database"
   type        = string
+  default     = "mtd"
 }
