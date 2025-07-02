70d797a0156ef2e3b5f748a8591aa58beeea02f5
HugoMD <95510299+HugoX2003@users.noreply.github.com>
2025-07-02 12:24:48 -0500

    Add Prometheus and Grafana monitoring stack
    Integrated Prometheus and Grafana into the Docker Compose setup for monitoring. Added Prometheus configuration file and updated Spring Boot properties to expose Prometheus metrics. Included Micrometer Prometheus registry dependency in pom.xml. Updated Terraform security group to allow Prometheus and Grafana ports.

--- terraform/resources.tf
+++ terraform/resources.tf
@@ -64,6 +64,21 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
